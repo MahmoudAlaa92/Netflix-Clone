bb67557f828e40651d1e629c803655996bfcd14f
Mahmoud Alaa <mahmoudalaa@192.168.1.3>
2024-06-14 00:20:24 +0300

    add TableViewCell and it’s CollectionView

--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -31,9 +31,7 @@ class MainTabBarViewController: UITabBarController {
         vc4.title = "Downlouds"
         
         tabBar.tintColor = .label
-        
     }
-
 
 }
 
---
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Mahmoud Alaa on 13/06/2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero ,collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        return collectionView
+    }()
+    
+    override func layoutSubviews() {
+        collectionView.frame = contentView.bounds
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+       
+        contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+extension CollectionViewTableViewCell: UICollectionViewDataSource ,UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for:indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+        
+}
